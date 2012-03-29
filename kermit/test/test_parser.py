0600e0112dc7757d540f765c630d00d46f32b569
Maciej Fijalkowski <fijall@gmail.com>
2012-03-29 10:37:49 +0200

    write the test

--- kermit/test/test_parser.py
+++ kermit/test/test_parser.py
@@ -1,5 +1,11 @@
 
-from kermit.sourceparser import parse
+from kermit.sourceparser import parse, Stmt, Block, ConstantInt, BinOp,\
+     Variable
 
-def test_parse():
-    parse('13;')
+def test_parse_basic():
+    assert parse('13;') == Block([Stmt(ConstantInt(13))])
+    assert parse('1 + 2;') == Block([Stmt(BinOp("+", ConstantInt(1),
+                                                ConstantInt(2)))])
+    assert parse('1 + a;') == Block([Stmt(BinOp('+', ConstantInt(1),
+                                                Variable('a')))])
+    
