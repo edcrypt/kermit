ee1e28192f9928c124f6720bf1e0526f7ad44e88
Maciej Fijalkowski <fijall@gmail.com>
2012-03-29 10:36:37 +0200

    add ast nodes

--- kermit/sourceparser.py
+++ kermit/sourceparser.py
@@ -7,6 +7,55 @@ grammar = py.path.local(kermitdir).join('grammar.txt').read("rt")
 regexs, rules, ToAST = parse_ebnf(grammar)
 _parse = make_parse_function(regexs, rules, eof=True)
 
+class Node(object):
+    """ The abstract AST node
+    """
+    def __eq__(self, other):
+        return (self.__class__ == other.__class__ and
+                self.__dict__ == other.__dict__)
+
+    def __ne__(self, other):
+        return not self == other
+
+class Block(Node):
+    """ A list of statements
+    """
+    def __init__(self, stmts):
+        self.stmts = stmts
+
+class Stmt(Node):
+    """ A single statement
+    """
+    def __init__(self, expr):
+        self.expr = expr
+
+class ConstantInt(Node):
+    """ Represent a constant
+    """
+    def __init__(self, intval):
+        self.intval = intval
+
+class BinOp(Node):
+    """ A binary operation
+    """
+    def __init__(self, op, left, right):
+        self.op = op
+        self.left = left
+        self.right = right
+
+class Variable(Node):
+    """ Variable reference
+    """
+    def __init__(self, varname):
+        self.varname = varname
+
+class Assignment(Node):
+    """ Assign to a variable
+    """
+    def __init__(self, varname, expr):
+        self.varname = varname
+        self.expr = expr
+
 class Transformer(object):
     """ Transforms AST from the obscure format given to us by the ennfparser
     to something easier to work with
