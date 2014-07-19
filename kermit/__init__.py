f9055381f6f659a5195d1a0d4964f68067732a40
James Mills <prologic@shortcircuit.net.au>
2014-07-19 20:50:52 +1000

    Added support for --help and --version

--- kermit/__init__.py
+++ kermit/__init__.py
@@ -1,3 +1,7 @@
-
 import os
+
+
+from .version import version as __version__  # noqa
+
+
 kermitdir = os.path.dirname(os.path.abspath(__file__))
---
+++ kermit/main.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+import sys
+
+
+from rpython.rlib.streamio import open_file_as_stream
+
+
+import kermit
+from kermit.interpreter import interpret
+
+
+USAGE = "kermit <filename>"
+VERSION = "kermit v" + kermit.__version__
+
+
+class Options(object):
+    """Options Object Container"""
+
+
+def parse_bool_arg(name, argv):
+    for i in xrange(len(argv)):
+        if argv[i] == name:
+            del(argv[i])
+            return True
+    return False
+
+
+def parse_arg(name, argv):
+    for i in xrange(len(argv)):
+        if argv[i] == name:
+            del(argv[i])
+            return argv.pop(i)
+    return ""
+
+
+def parse_args(argv):
+    opts = Options()
+
+    opts.help = parse_bool_arg("--help", argv)
+    opts.version = parse_bool_arg("--version", argv)
+
+    if opts.help:
+        print USAGE
+        raise SystemExit(0)
+
+    if opts.version:
+        print VERSION
+        raise SystemExit(0)
+
+    del argv[0]
+
+    if not argv:
+        print USAGE
+        raise SystemExit(1)
+
+    return opts, argv
+
+
+def main(argv):
+    try:
+        opts, files = parse_args(argv)
+
+        return run(files, opts)
+    except SystemExit:
+        return 0
+
+
+def run(files, opts):
+    for file in files:
+        f = open_file_as_stream(file)
+        data = f.readall()
+        f.close()
+        interpret(data)
+
+    return 0
+
+
+def entrypoint():
+    """setuptools Entry Point"""
+
+    return main(sys.argv)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    entrypoint()
